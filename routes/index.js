4639733a84d59e6a218249968a33966c61a07afc
clareysean <seanpclarey@gmail.com>
2023-07-11 15:20:41 -0400

    update copy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Dev Skills Page:" });
+  res.render("index", { title: "Dev Skills Page" });
 });
 
 module.exports = router;
