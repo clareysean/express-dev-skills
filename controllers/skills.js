483f416136b7c582d7c38c2038998cba52388e5c
clareysean <seanpclarey@gmail.com>
2023-07-12 21:34:58 -0400

    Update delete functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,6 +1,6 @@
 const Skill = require("../models/skill.js");
 
-module.exports = { index, show };
+module.exports = { index, show, new: newSkill, create, delete: deleteSkill };
 
 function index(req, res) {
   res.render("skills/index", {
@@ -15,3 +15,20 @@ function show(req, res) {
     title: 'Skill Details'
   });
 }
+
+function newSkill(req, res) {
+  res.render('skills/new', { title: 'New Skill' });
+}
+
+function create(req, res) {
+  console.log(req.body);
+  // The model is responsible for creating data
+  Skill.create(req.body);
+  // Do a redirect anytime data is changed
+  res.redirect('/skills');
+}
+
+function deleteSkill(req,res){
+  Skill.deleteOne(req.params.id);
+  res.redirect('/skills');
+ }
--- models/skill.js
+++ models/skill.js
@@ -22,6 +22,8 @@ const skills = [
 module.exports = {
   getAll,
   getOne,
+  create,
+  deleteOne
 };
 
 function getAll() {
@@ -31,4 +33,19 @@ function getAll() {
 function getOne(id) {
   id = parseInt(id);
   return skills.find((skill) => skill.id === id);
+}
+
+function create(skill){
+  skill.id = Date.now() % 1000000;
+  skills.push(skill);
+  console.log(skill)
+  console.log(skills)
+}
+
+function deleteOne(id) {
+  // All properties attached to req.params are strings!
+  id = parseInt(id);
+  // Find the index based on the id of the todo object
+  const idx = skills.findIndex(skill => skill.id === id);
+  skills.splice(idx, 1);
 }
